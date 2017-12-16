c3194e15c57b231a282aa1a233ccc093c61ba3f4
Leticia <leticia.power.66@gmail.com>
2017-12-16 15:24:15 -0200

    role

--- app/src/main/java/br/com/siscarros/app/controller/RoleController.java
+++ app/src/main/java/br/com/siscarros/app/controller/RoleController.java
@@ -1,7 +1,27 @@
 package br.com.siscarros.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.siscarros.app.entities.Role;
+import br.com.siscarros.app.service.RoleServiceInt;
+
 public class RoleController {
 	
+	@Autowired
+	private RoleServiceInt roleService;
 	
+	@GetMapping(path="/listaRole")
+	public List<Role> ListaRole () {
+			
+	return roleService.ListaTodos();	
+		
+	}
+	
+	@GetMapping(path="/buscaPorNome")
+	public void BuscaPorNome () { 	
+	}
 
 }
